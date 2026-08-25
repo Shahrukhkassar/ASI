06fa1d5f828f0ba6e5ee673aeca6bae226c8e8b7
Shahrukhkassar <116454412+Shahrukhkassar@users.noreply.github.com>
2026-08-25 14:56:17 +0530

    Refactor vite.config.ts for cleaner syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
-hereimport tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-export default defineConfig(() => {
-  return {
-    base: './',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    build: {
-      chunkSizeWarningLimit: 2000
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
-});
+export default defineConfig({
+  base: './',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.')
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 2000
+  }
+})
